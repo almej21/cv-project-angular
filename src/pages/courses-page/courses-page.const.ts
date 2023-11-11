69ac31439d5c4c0073e205acec73a54d0c5128c1
almej21 <almogaharon21@gmail.com>
2023-11-11 17:27:42 +0200

    design fixes

--- src/pages/courses-page/courses-page.const.ts
+++ src/pages/courses-page/courses-page.const.ts
@@ -5,21 +5,16 @@ export const COURSES_DATA: {
   {
     label: 'fullstack course',
     description:
-      'This is my main Github link, where you can find all of my personal projects.',
+      'Full-Stack Web Developer with just ONE course. HTML, CSS, Javascript, Node, React, PostgreSQL, Web3 and DApps.',
   },
   {
-    label: 'remote sessions',
+    label: 'node.js',
     description:
-      'Web app, based on node.js and mongoDB on the backend and react on the frontend. create remote sessions for mentors and students for practicing code',
+      'Through our Node.js course, I learned the art of server-side JavaScript, delving into asynchronous programming techniques and honing the skills needed to construct scalable web applications. This hands-on experience empowered me to create dynamic server-side solutions, unlocking the full potential of the Node.js runtime environment.',
   },
   {
-    label: 'chat application - client',
+    label: 'JWT and web socket technologies',
     description:
-      'based on node.js and mongoDB and react, this is the client side of the sports updates and chat application between friends.',
-  },
-  {
-    label: 'chat application - server',
-    description:
-      'based on node.js and mongoDB and react, this is the backend side of the sports updates and chat application between friends.',
+      'From the JWT and Socket.io course, I have mastered secure user authentication with JWT and learned to create dynamic, real-time features using Socket.io for a seamless web experience.',
   },
 ];
